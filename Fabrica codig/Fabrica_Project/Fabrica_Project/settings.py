63285ae9063ccca37f61896be3447d2ca00c9c94
saularana <saularana201831189@cunoc.edu.gt>
2024-01-23 11:36:08 -0600

    ya se creo la plantilla inicial y se agrego bootstrap

--- Fabrica codig/Fabrica_Project/Fabrica_Project/settings.py
+++ Fabrica codig/Fabrica_Project/Fabrica_Project/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'Fabrica_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'Templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
@@ -121,7 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
