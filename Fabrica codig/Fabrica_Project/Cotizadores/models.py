8f577b5e70072df07e3baf553f4c71cb2d0215e5
GOrtiz04 <99684632+GOrtiz04@users.noreply.github.com>
2024-01-25 11:00:36 -0600

    backend

--- Fabrica codig/Fabrica_Project/Cotizadores/models.py
+++ Fabrica codig/Fabrica_Project/Cotizadores/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from Asesores.models import Presupuesto
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Cotizacion(models.Model):
+    cotizador = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False)
+    presupuesto = models.ForeignKey(Presupuesto, on_delete=models.PROTECT, null=False, blank=False)
+    fecha = models.DateTimeField(auto_now_add=True)
+    archivo = models.FileField(null=False, blank=False)
