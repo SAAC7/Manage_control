1f468acfca70c6455f1fdc34267ac3c7c72019ed
saularana <saularana201831189@cunoc.edu.gt>
2024-01-31 16:01:43 -0600

    Guardar cambios locales en db.sqlite3

--- Fabrica codig/Fabrica_Project/Cotizadores/models.py
+++ Fabrica codig/Fabrica_Project/Cotizadores/models.py
@@ -6,14 +6,8 @@ from django.contrib.auth.models import User
 
 class Cotizacion(models.Model):
     cotizador = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False)
-    presupuesto = models.ForeignKey(Presupuesto, on_delete=models.PROTECT, null=False, blank=False)
+    diseno = models.ForeignKey(Diseno, on_delete=models.PROTECT, null=True, blank=False)
     fecha = models.DateTimeField(auto_now_add=True)
     estado=models.TextField(null=True, blank=True, default='Esperando aprobación')
     archivo = models.FileField(upload_to='cotizaciones/', null=False, blank=False)
 
-# class Reservacion(models.Model):
-#     fecha_de_reserva = models.DateTimeField(auto_now_add=True)
-#     diseño = models.ForeignKey(Diseno, on_delete=models.PROTECT, null=False, blank=False)
-#     cotizacion=models.ForeignKey(Cotizacion, on_delete=models.PROTECT, null=True, blank=True)
-
-    
