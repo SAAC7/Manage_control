4d857747cad30711c70534197f2b004cc661d58e
saularana <saularana201831189@cunoc.edu.gt>
2024-02-10 12:52:05 -0600

    se encontro bug que no se puede subir .txt vacios

--- Fabrica codig/Fabrica_Project/Produccion/views.py
+++ Fabrica codig/Fabrica_Project/Produccion/views.py
@@ -7,42 +7,62 @@ from django.http import HttpResponse
 from .models import Orden_trabajo,Trabajo,Diseno_CNC,Trabajos_Orden
 from Asesores.models import Presupuesto, Diseno
 from Cotizadores.models import Cotizacion
-from .models import Orden_trabajo
+
 
 
 #Lista de ordenes de trabajo disponibles
 @login_required(login_url='index')
 def ordenes_listado(request):
     user = request.user
+    lista_Ordenes=[]
     if user.groups.filter(name='Asesor').exists():
-        presupuestos_aprobados = Presupuesto.objects.filter(
-            asesor=user,
-            diseno__estado="Aprobado",
-            diseno__cotizacion__estado="Aprobado",
-            orden_trabajo__isnull=False            
-        ).distinct()
-
+        ordenes = Orden_trabajo.objects.filter(presupuesto__asesor=user)
+        for orden in ordenes:
+        # Obtiene el diseño aprobado relacionado con la orden de trabajo, si existe
+            diseno_aprobado = Diseno.objects.filter(presupuesto=orden.presupuesto, estado='Aprobado').first()       # Obtiene la cotización aprobada relacionada con el diseño, si existe
+        if diseno_aprobado:
+            cotizacion_aprobada = Cotizacion.objects.filter(diseno=diseno_aprobado, estado='Aprobado').first()
+        else:
+            cotizacion_aprobada = None          
+        # Crea un diccionario con la información y lo añade a la lista de objetos
+        objeto = {
+            'orden': orden,
+            'diseno_aprobado': diseno_aprobado,
+            'cotizacion_aprobada': cotizacion_aprobada
+        }
+        lista_Ordenes.append(objeto)
         return render(
-            request,
-            'Produccion/Ordenes_trabajo.html',
-            {'presupuesto_diseno_cotizacion': presupuestos_aprobados}
+        request,
+        'Produccion/Ordenes_trabajo.html',
+        {'lista_Ordenes': lista_Ordenes}
         )
     elif (user.groups.filter(name='Administrador').exists() or user.is_superuser):
-        presupuestos_aprobados = Presupuesto.objects.filter(
-            diseno__estado="Aprobado",
-            diseno__cotizacion__estado="Aprobado",
-            orden_trabajo__isnull=False 
-        ).distinct()
-
+        ordenes = Orden_trabajo.objects.all()
+        for orden in ordenes:
+        # Obtiene el diseño aprobado relacionado con la orden de trabajo, si existe
+            diseno_aprobado = Diseno.objects.filter(presupuesto=orden.presupuesto, estado='Aprobado').first()       # Obtiene la cotización aprobada relacionada con el diseño, si existe
+        if diseno_aprobado:
+            cotizacion_aprobada = Cotizacion.objects.filter(diseno=diseno_aprobado, estado='Aprobado').first()
+        else:
+            cotizacion_aprobada = None          
+        # Crea un diccionario con la información y lo añade a la lista de objetos
+        objeto = {
+            'orden': orden,
+            'diseno_aprobado': diseno_aprobado,
+            'cotizacion_aprobada': cotizacion_aprobada
+        }
+        lista_Ordenes.append(objeto)
         return render(
-            request,
-            'Produccion/Ordenes_trabajo.html',
-            {'presupuesto_diseno_cotizacion': presupuestos_aprobados}
-            )
+        request,
+        'Produccion/Ordenes_trabajo.html',
+        {'lista_Ordenes': lista_Ordenes}
+        )
+    
+        
     else:
         error = "No tienes permiso para acceder a esta página."
         return render(request, '404.html', {'error': error})
-        
+@login_required(login_url='index')        
 def trabajo(request):
     usuario_autenticado = request.user
     
@@ -55,7 +75,7 @@ def trabajo(request):
     else:
         error = "No tienes permiso para acceder a esta página."
         return render(request, '404.html', {'error': error})
-    
+@login_required(login_url='index')    
 def trabajofin(request):
     usuario_autenticado = request.user
     if not usuario_autenticado.groups.filter(name__in=['Administrador', 'Designer', 'Asesor', 'Cotizador']).exists() or not user.is_superuser:
@@ -64,6 +84,18 @@ def trabajofin(request):
     elif (user.groups.filter(name='Administrador').exists() or user.is_superuser):
         trabajos = Trabajo.objects.filter(fecha_fin__isnull=False)
         return render(request, 'Produccion/trabajos_finalizados.html', {'trabajos': trabajos})
+    else:
+        error = "No tienes permiso para acceder a esta página."
+        return render(request, '404.html', {'error': error})
+    
+    
+    
+@login_required(login_url='index')    
+def ordenes_info(request,or_id):
+    user= request.user
+    if (user.groups.filter(name='Administrador').exists() or user.is_superuser):
+        cotizacion = get_object_or_404(Cotizacion,pk=or_id)
+        return render(request, 'Produccion/Ordenes_trabajo.html')
     else:
         error = "No tienes permiso para acceder a esta página."
         return render(request, '404.html', {'error': error})
