b95ccef1840a909483f78cd32061b494c0dba142
saularana <saularana201831189@cunoc.edu.gt>
2024-02-15 10:01:43 -0600

    se arreglo los finalizados de trabajos y de cotizaciones

--- Fabrica codig/Fabrica_Project/Produccion/views.py
+++ Fabrica codig/Fabrica_Project/Produccion/views.py
@@ -65,12 +65,16 @@ def trabajo(request):
 @login_required(login_url='index')    
 def trabajofin(request):
     usuario_autenticado = request.user
-    if not usuario_autenticado.groups.filter(name__in=['Administrador', 'Designer', 'Asesor', 'Cotizador']).exists() or not user.is_superuser:
-        trabajos_listado = Trabajo.objects.filter(trabajador=usuario_autenticado,fecha_fin__isnull=False)
-        return render(request, 'Produccion/trabajos_finalizados.html', {'trabajos': trabajos})
-    elif (user.groups.filter(name='Administrador').exists() or user.is_superuser):
-        trabajos = Trabajo.objects.filter(fecha_fin__isnull=False)
-        return render(request, 'Produccion/trabajos_finalizados.html', {'trabajos': trabajos})
+    if not (usuario_autenticado.groups.filter(name__in=['Administrador', 'Designer', 'Asesor', 'Cotizador']).exists() or usuario_autenticado.is_superuser):
+        # trabajos_listado = Trabajo.objects.filter(trabajador=usuario_autenticado,fecha_fin__isnull=False)
+        trabajos_listado = Trabajos_Orden.objects.filter(trabajo__trabajador=usuario_autenticado,trabajo__fecha_fin__isnull=False)
+        print("1")
+        return render(request, 'Produccion/trabajos_finalizados.html', {'trabajos': trabajos_listado})
+    elif (usuario_autenticado.groups.filter(name='Administrador').exists() or usuario_autenticado.is_superuser):
+        # trabajos_listado = Trabajo.objects.filter(fecha_fin__isnull=False)
+        trabajos_listado = Trabajos_Orden.objects.filter(trabajo__fecha_fin__isnull=False)
+        print("2")
+        return render(request, 'Produccion/trabajos_finalizados.html', {'trabajos': trabajos_listado})
     else:
         error = "No tienes permiso para acceder a esta página."
         return render(request, '404.html', {'error': error})
